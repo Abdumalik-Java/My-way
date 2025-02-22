2f3ce76ce199a28421ae0279f77b64c02b022757
Abdumalik-Java <abdumalikbakhtiyorovwork@gmail.com>
2025-02-22 14:07:20 +0500

    Project started by Abdumalik-Java

---
+++ src/main/java/com/example/webjpahackathon/model/Car.java
@@ -0,0 +1,26 @@
+package com.example.webjpahackathon.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String model;
+    @Column(nullable = false)
+    private String category;
+
+}
